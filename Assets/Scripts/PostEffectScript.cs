f4954c3f956d3a5cfed2d2a21d102092950f4647
maslychm <maslychm@gmail.com>
2019-11-22 19:56:03 -0500

    added screen post processing shader

---
+++ Assets/Scripts/PostEffectScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PostEffectScript : MonoBehaviour
+{
+    public Material shaderMat;
+
+    void OnRenderImage(RenderTexture src, RenderTexture dest)
+    {
+        Graphics.Blit(src, dest, shaderMat);
+    }
+}
