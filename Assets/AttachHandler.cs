841bd78686f220dd1bbb7639c4227017def56827
maslychm <43874961+maslychm@users.noreply.github.com>
2019-12-09 22:19:49 -0500

    windows and skybox

--- Assets/AttachHandler.cs
+++ Assets/AttachHandler.cs
@@ -8,14 +8,22 @@ public class AttachHandler : MonoBehaviour
     GameObject pedestal;
     Vector3 lockPosition;
     GameObject barySphere;
-    bool rEnabled;
+    bool nowAttached;
+    Quaternion rot;
+    Vector3 sc;
+    Color originalColor;
+    GameObject p1;
+    GameObject p2;
 
     void Start()
     {
         pedestal = gameObject;
         barySphere = GameObject.Find("BarySphere");
         lockPosition = pedestal.transform.position + pedestalOffset;
-        rEnabled = true;
+        nowAttached = true;
+        p1 = GameObject.Find("Pedestal");
+        p2 = GameObject.Find("Pedestal (1)");
+        p1.GetComponent<Material>().SetColor("_Color", Color.white);
     }
 
     void Update()
@@ -23,7 +31,19 @@ public class AttachHandler : MonoBehaviour
         float dist = Vector3.Distance(lockPosition, barySphere.transform.position);
         Debug.Log(dist);
 
-        if (rEnabled)
+        if (nowAttached)
+        {
+            if (dist <= 1f)
+            {
+
+            }
+            else
+            {
+                //barySphere.transform.localScale = sc;
+                nowAttached = false;
+            }
+        }
+        else
         {
             if (dist <= 1f)
             {
@@ -31,13 +51,11 @@ public class AttachHandler : MonoBehaviour
                 barySphere.GetComponent<Rigidbody>().isKinematic = true;
                 barySphere.transform.position = lockPosition;
                 barySphere.transform.parent = pedestal.transform;
+                nowAttached = true;
             }
-        }
-        else
-        {
-            if (dist >= 1f)
+            else
             {
-                
+
             }
         }
     }
