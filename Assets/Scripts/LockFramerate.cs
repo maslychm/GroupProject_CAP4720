fb21d6f752da41a60fbe3633fb97609e325c2a4e
maslychm <43874961+maslychm@users.noreply.github.com>
2019-12-10 00:51:30 -0500

    locked framerate

---
+++ Assets/Scripts/LockFramerate.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LockFramerate : MonoBehaviour
+{
+    public int target = 60;
+
+    void Start()
+    {
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = target;
+    }
+    
+    void Update()
+    {
+        if (Application.targetFrameRate != target)
+        {
+            Application.targetFrameRate = target;
+        }
+    }
+}
